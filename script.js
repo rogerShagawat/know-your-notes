a4da8b88b4290d6cc40318aa077b09338c943781
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-11-02 13:55:45 -0400

    added isCorrect function

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-
 function generateRandomNote() {
-    //Returns a number
-    //E.g. 65 (which represents A)
-    return (Math.floor(Math.random() * 7) + 97)
+   //Returns a number
+   //E.g. 65 (which represents A)
+   return Math.floor(Math.random() * 7) + 97;
+}
+
+function isCorrect(selectedNote) {
+   const noteSrc = document.getElementById("note-image").src;
+   const correctNote = noteSrc.split("/").pop().split("-")[0];
+   if (selectedNote !== correctNote) {
+      return false;
+   }
+   return true;
 }
 
 window.onload = function getRandomImageForClef() {
-    noteName = String.fromCharCode(generateRandomNote())
-    clef = "treble"
-    console.log(noteName)
-    document.getElementById("note-image").src = `assets/${noteName}-${clef}.png`;
-}
-
+   noteName = String.fromCharCode(generateRandomNote());
+   clef = "treble";
+   console.log(noteName);
+   document.getElementById("note-image").src = `assets/${noteName}-${clef}.png`;
+};
