cfd4008cee0beb71586d525026911f4b07bc58d7
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-11-08 11:28:55 -0500

    added non-functional settings button

--- script.js
+++ script.js
@@ -13,15 +13,20 @@ function isCorrect(selectedNote) {
    return true;
 }
 
-function resetButtons() {}
+function resetButtons() {
+   // TODO reset the CSS of the buttons when failed
+}
 
 function runOnCorrect() {
    console.log("correct!");
    getRandomImageForClef();
+   resetButtons();
+   // TODO should display on screen positive affirmation
 }
 
 function runOnIncorrect() {
    console.log("failure!");
+   // TODO change css of the buttons so that they're greyed out when gotten wrong
 }
 
 function onNoteSelection(event) {
@@ -43,3 +48,21 @@ window.onload = getRandomImageForClef();
 
 var parent = document.getElementById("note-buttons");
 parent.addEventListener("click", onNoteSelection);
+
+function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+ }
+ 
+ // Close the dropdown menu if the user clicks outside of it
+//  window.onclick = function(event) {
+//    if (!event.target.matches('.dropbtn')) {
+//      var dropdowns = document.getElementsByClassName("dropdown-content");
+//      var i;
+//      for (i = 0; i < dropdowns.length; i++) {
+//        var openDropdown = dropdowns[i];
+//        if (openDropdown.classList.contains('show')) {
+//          openDropdown.classList.remove('show');
+//        }
+//      }
+//    }
+//  }
